af1de480502efe87cd09b0773f1f4e92f44f8b6f
Vineet Chhikara <vinitchhikara12@gmail.com>
2025-10-03 21:09:26 +0530

    Interleave Question using stack and Queue

--- Queue/interleave1sthalf.cpp
+++ Queue/interleave1sthalf.cpp
@@ -1,68 +1,97 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-#include<queue>
-#include<stack>
+#include <queue>
+#include <stack>
 
-
-void print(queue<int> q){
-    if(q.empty()){
+void print(queue<int> q)
+{
+    if (q.empty())
+    {
         return;
     }
-    while(!q.empty()){
-        cout<<q.front()<<" ";
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
         q.pop();
-    } cout<<endl;
-
-}
-
-// interleave the first half of queue with second half queue whose size is even
-void interleave(queue<int> &q){
-    if(q.empty()){
-        return;
     }
-    stack<int> s;
+    cout << endl;
+}
+// interleave the first half of queue with second half queue whose size is even
+void interleaveQueue(queue<int> &q){
+    if(q.empty()) return;
+
+    queue<int> q2;
     int size = q.size();
     int i=0;
-    while(i<(size)/2){
+    while(i++<size/2){
+        q2.push(q.front());
+        q.pop();
+    }
+
+    while(!q2.empty()){
+        q.push(q2.front());
+        q2.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    return;
+}
+void interleave(queue<int> &q)
+{
+    if (q.empty())
+    {
+        return;
+    }
+    // using stack
+    stack<int> s;
+    int size = q.size();
+    int i = 0;
+    while (i < (size) / 2)
+    {
         s.push(q.front());
         q.pop();
         i++;
     }
-    while(!s.empty()){
+    while (!s.empty())
+    {
         q.push(s.top());
         s.pop();
     }
-    i=0;
-    while(i<(size)/2){
+    i = 0;
+    while (i < (size) / 2)
+    {
         q.push(q.front());
         q.pop();
         i++;
     }
     i = 0;
-    while(i<(size)/2){
+    while (i < (size) / 2)
+    {
         s.push(q.front());
         q.pop();
         i++;
     }
-    while(!s.empty()){
+    while (!s.empty())
+    {
         q.push(s.top());
         s.pop();
         q.push(q.front());
         q.pop();
     }
-
 }
-int main(){
+int main()
+{
     queue<int> q;
-    q.push(11); 
-    q.push(12); 
-    q.push(13); 
-    q.push(14); 
-    q.push(15); 
-    q.push(16); 
-    q.push(17); 
+    q.push(11);
+    q.push(12);
+    q.push(13);
+    q.push(14);
+    q.push(15);
+    q.push(16);
+    q.push(17);
     q.push(18);
     print(q);
-    interleave(q);
+    // interleave(q); //using Stack
+    interleaveQueue(q); //using Queue
     print(q);
 }
